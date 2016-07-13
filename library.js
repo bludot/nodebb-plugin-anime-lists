66419c3c337b75842b8fc26131525d253a5b2ad3
DESKTOP-PQJD2BL\bobet <admin@bludotos.com>
2016-07-13 18:04:37 -0400

    initial upload

---
+++ library.js
@@ -0,0 +1,374 @@
+"use strict";
+
+var db = module.parent.require('./database'),
+    hotswap = module.parent.require('./hotswap');
+
+var nconf = module.parent.require('nconf'),
+    async = module.parent.require('async'),
+    mkdirp = module.parent.require('mkdirp'),
+    winston = module.parent.require('winston'),
+    express = module.parent.require('express');
+
+var fs = require('fs'),
+    path = require('path');
+
+var plugin = {};
+
+var escaperegex = function(string) {
+  return string.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+};
+
+function addslashes( str ) {
+    return (str + '').replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
+}
+var encodeHtmlEntity = function(str) {
+  var buf = [];
+  for (var i=str.length-1;i>=0;i--) {
+    buf.unshift(['&#', str[i].charCodeAt(), ';'].join(''));
+  }
+  return buf.join('');
+};
+
+var decodeHtmlEntity = function(str) {
+  return str.replace(/&#(\d+);/g, function(match, dec) {
+    return String.fromCharCode(dec);
+  });
+};
+
+var HtmlEntities = function() {};
+
+HtmlEntities.map = {
+    "'": "&apos;",
+    "<": "&lt;",
+    ">": "&gt;",
+    " ": "&nbsp;",
+    "¡": "&iexcl;",
+    "¢": "&cent;",
+    "£": "&pound;",
+    "¤": "&curren;",
+    "¥": "&yen;",
+    "¦": "&brvbar;",
+    "§": "&sect;",
+    "¨": "&uml;",
+    "©": "&copy;",
+    "ª": "&ordf;",
+    "«": "&laquo;",
+    "¬": "&not;",
+    "®": "&reg;",
+    "¯": "&macr;",
+    "°": "&deg;",
+    "±": "&plusmn;",
+    "²": "&sup2;",
+    "³": "&sup3;",
+    "´": "&acute;",
+    "µ": "&micro;",
+    "¶": "&para;",
+    "·": "&middot;",
+    "¸": "&cedil;",
+    "¹": "&sup1;",
+    "º": "&ordm;",
+    "»": "&raquo;",
+    "¼": "&frac14;",
+    "½": "&frac12;",
+    "¾": "&frac34;",
+    "¿": "&iquest;",
+    "À": "&Agrave;",
+    "Á": "&Aacute;",
+    "Â": "&Acirc;",
+    "Ã": "&Atilde;",
+    "Ä": "&Auml;",
+    "Å": "&Aring;",
+    "Æ": "&AElig;",
+    "Ç": "&Ccedil;",
+    "È": "&Egrave;",
+    "É": "&Eacute;",
+    "Ê": "&Ecirc;",
+    "Ë": "&Euml;",
+    "Ì": "&Igrave;",
+    "Í": "&Iacute;",
+    "Î": "&Icirc;",
+    "Ï": "&Iuml;",
+    "Ð": "&ETH;",
+    "Ñ": "&Ntilde;",
+    "Ò": "&Ograve;",
+    "Ó": "&Oacute;",
+    "Ô": "&Ocirc;",
+    "Õ": "&Otilde;",
+    "Ö": "&Ouml;",
+    "×": "&times;",
+    "Ø": "&Oslash;",
+    "Ù": "&Ugrave;",
+    "Ú": "&Uacute;",
+    "Û": "&Ucirc;",
+    "Ü": "&Uuml;",
+    "Ý": "&Yacute;",
+    "Þ": "&THORN;",
+    "ß": "&szlig;",
+    "à": "&agrave;",
+    "á": "&aacute;",
+    "â": "&acirc;",
+    "ã": "&atilde;",
+    "ä": "&auml;",
+    "å": "&aring;",
+    "æ": "&aelig;",
+    "ç": "&ccedil;",
+    "è": "&egrave;",
+    "é": "&eacute;",
+    "ê": "&ecirc;",
+    "ë": "&euml;",
+    "ì": "&igrave;",
+    "í": "&iacute;",
+    "î": "&icirc;",
+    "ï": "&iuml;",
+    "ð": "&eth;",
+    "ñ": "&ntilde;",
+    "ò": "&ograve;",
+    "ó": "&oacute;",
+    "ô": "&ocirc;",
+    "õ": "&otilde;",
+    "ö": "&ouml;",
+    "÷": "&divide;",
+    "ø": "&oslash;",
+    "ù": "&ugrave;",
+    "ú": "&uacute;",
+    "û": "&ucirc;",
+    "ü": "&uuml;",
+    "ý": "&yacute;",
+    "þ": "&thorn;",
+    "ÿ": "&yuml;",
+    "Œ": "&OElig;",
+    "œ": "&oelig;",
+    "Š": "&Scaron;",
+    "š": "&scaron;",
+    "Ÿ": "&Yuml;",
+    "ƒ": "&fnof;",
+    "ˆ": "&circ;",
+    "˜": "&tilde;",
+    "Α": "&Alpha;",
+    "Β": "&Beta;",
+    "Γ": "&Gamma;",
+    "Δ": "&Delta;",
+    "Ε": "&Epsilon;",
+    "Ζ": "&Zeta;",
+    "Η": "&Eta;",
+    "Θ": "&Theta;",
+    "Ι": "&Iota;",
+    "Κ": "&Kappa;",
+    "Λ": "&Lambda;",
+    "Μ": "&Mu;",
+    "Ν": "&Nu;",
+    "Ξ": "&Xi;",
+    "Ο": "&Omicron;",
+    "Π": "&Pi;",
+    "Ρ": "&Rho;",
+    "Σ": "&Sigma;",
+    "Τ": "&Tau;",
+    "Υ": "&Upsilon;",
+    "Φ": "&Phi;",
+    "Χ": "&Chi;",
+    "Ψ": "&Psi;",
+    "Ω": "&Omega;",
+    "α": "&alpha;",
+    "β": "&beta;",
+    "γ": "&gamma;",
+    "δ": "&delta;",
+    "ε": "&epsilon;",
+    "ζ": "&zeta;",
+    "η": "&eta;",
+    "θ": "&theta;",
+    "ι": "&iota;",
+    "κ": "&kappa;",
+    "λ": "&lambda;",
+    "μ": "&mu;",
+    "ν": "&nu;",
+    "ξ": "&xi;",
+    "ο": "&omicron;",
+    "π": "&pi;",
+    "ρ": "&rho;",
+    "ς": "&sigmaf;",
+    "σ": "&sigma;",
+    "τ": "&tau;",
+    "υ": "&upsilon;",
+    "φ": "&phi;",
+    "χ": "&chi;",
+    "ψ": "&psi;",
+    "ω": "&omega;",
+    "ϑ": "&thetasym;",
+    "ϒ": "&Upsih;",
+    "ϖ": "&piv;",
+    "–": "&ndash;",
+    "—": "&mdash;",
+    "‘": "&lsquo;",
+    "’": "&rsquo;",
+    "‚": "&sbquo;",
+    "“": "&ldquo;",
+    "”": "&rdquo;",
+    "„": "&bdquo;",
+    "†": "&dagger;",
+    "‡": "&Dagger;",
+    "•": "&bull;",
+    "…": "&hellip;",
+    "‰": "&permil;",
+    "′": "&prime;",
+    "″": "&Prime;",
+    "‹": "&lsaquo;",
+    "›": "&rsaquo;",
+    "‾": "&oline;",
+    "⁄": "&frasl;",
+    "€": "&euro;",
+    "ℑ": "&image;",
+    "℘": "&weierp;",
+    "ℜ": "&real;",
+    "™": "&trade;",
+    "ℵ": "&alefsym;",
+    "←": "&larr;",
+    "↑": "&uarr;",
+    "→": "&rarr;",
+    "↓": "&darr;",
+    "↔": "&harr;",
+    "↵": "&crarr;",
+    "⇐": "&lArr;",
+    "⇑": "&UArr;",
+    "⇒": "&rArr;",
+    "⇓": "&dArr;",
+    "⇔": "&hArr;",
+    "∀": "&forall;",
+    "∂": "&part;",
+    "∃": "&exist;",
+    "∅": "&empty;",
+    "∇": "&nabla;",
+    "∈": "&isin;",
+    "∉": "&notin;",
+    "∋": "&ni;",
+    "∏": "&prod;",
+    "∑": "&sum;",
+    "−": "&minus;",
+    "∗": "&lowast;",
+    "√": "&radic;",
+    "∝": "&prop;",
+    "∞": "&infin;",
+    "∠": "&ang;",
+    "∧": "&and;",
+    "∨": "&or;",
+    "∩": "&cap;",
+    "∪": "&cup;",
+    "∫": "&int;",
+    "∴": "&there4;",
+    "∼": "&sim;",
+    "≅": "&cong;",
+    "≈": "&asymp;",
+    "≠": "&ne;",
+    "≡": "&equiv;",
+    "≤": "&le;",
+    "≥": "&ge;",
+    "⊂": "&sub;",
+    "⊃": "&sup;",
+    "⊄": "&nsub;",
+    "⊆": "&sube;",
+    "⊇": "&supe;",
+    "⊕": "&oplus;",
+    "⊗": "&otimes;",
+    "⊥": "&perp;",
+    "⋅": "&sdot;",
+    "⌈": "&lceil;",
+    "⌉": "&rceil;",
+    "⌊": "&lfloor;",
+    "⌋": "&rfloor;",
+    "⟨": "&lang;",
+    "⟩": "&rang;",
+    "◊": "&loz;",
+    "♠": "&spades;",
+    "♣": "&clubs;",
+    "♥": "&hearts;",
+    "♦": "&diams;"
+};
+
+HtmlEntities.decode = function(string) {
+    var entityMap = HtmlEntities.map;
+    for (var key in entityMap) {
+        var entity = entityMap[key];
+        var regex = new RegExp(entity, 'g');
+        string = string.replace(regex, key);
+    }
+    string = string.replace(/&quot;/g, '"');
+    string = string.replace(/&amp;/g, '&');
+    return string;
+}
+
+var getMarks = function(callback) {
+		if (plugin.cache) {
+			return callback(null, plugin.cache);
+		} else {
+			db.get('plugins:anime-list', function(err, data) {
+				try {
+					var replacements = JSON.parse(data);
+
+					if (replacements == null) {
+						replacements = [];
+					}
+
+					// Eliminate errors in route definition
+					plugin.cache = replacements.map(function(obj) {
+						obj.json_string = JSON.stringify(obj);
+						return obj;
+					});
+					console.log(replacements);
+
+					callback(null, plugin.cache);
+				} catch (err) {
+					callback(err);
+				}
+			});
+		}
+	};
+
+var renderAdmin = function(req, res) {
+	getMarks(function(err, data) {
+		res.render('admin/admin-page', {
+			data: data
+		});
+	});
+};
+var Markdown_ext = {
+
+    addAdminNavigation: function(header, callback) {
+        header.plugins.push({
+            route: '/admin-page',
+            icon: 'fa-mobile',
+            name: 'anime-list'
+        });
+
+        callback(null, header);
+    },
+    admin: {
+        menu: function(custom_header, callback) {
+            custom_header.plugins.push({
+                "route": '/admin-page',
+                "icon": 'fa-edit',
+                "name": 'anime-list'
+            });
+
+            callback(null, custom_header);
+        }
+    },
+	init: function(params, callback) {
+        var app = params.router,
+            middleware = params.middleware;
+
+        app.get('/admin/admin-page', middleware.admin.buildHeader, renderAdmin);
+        app.get('/api/admin/admin-page', renderAdmin);
+
+        var SocketAdmin = module.parent.require('./socket.io/admin');
+        SocketAdmin.settings.saveMarks = function(socket, data, callback) {
+            delete plugin.cache;
+
+            async.series([
+                async.apply(db.set, 'plugins:anime-list', JSON.stringify(data))
+            ], callback);
+        };
+		callback();
+        //this.reloadRoutes(middleware, callback);
+    }
+};
+
+module.exports = Markdown_ext;
---
+++ templates/admin/admin-page.tpl
@@ -0,0 +1,145 @@
+<div class="row">
+    <div class="col-sm-2 col-xs-12 settings-header">Marks:</div>
+    <div class="col-sm-10 col-xs-12">
+        <p class="lead">
+            Define and customise your markdown codes
+        </p>
+        <p>
+            use "\" in your regex. it is put in as <code>new RegExp(regex)</code>
+        </p>
+            <p>
+        <div id="lists">
+  <div class="toolbar">
+    <span>Type: </span>
+    <select>
+      <option value="0">All</option>
+      <option value="1">Anime</option>
+      <option value="2">Manga</option>
+    </select>
+    <span>Sort: </span>
+    <select>
+      <option value="1">test 1</option>
+      <option value="2">test 2</option>
+      <option value="3">test 3</option>
+      <option value="4">test 4</option>
+      <option value="5">test 5</option>
+    </select>
+    <div class="buttons">
+      <span class="button"><i class="fa fa-list"></i></span>
+      <span class="button selected"><i class="fa fa-th"></i></span>
+    </div>
+  </div>
+
+
+  <div class="grid">
+      <div class="grid-item">
+      <div class="inner">
+
+        <div class="image" style="cursor:pointer;">
+          +
+        </div>
+        <h3>
+    Title
+    </h3>
+      </div>
+    </div>
+    <!-- BEGIN data -->
+    <div class="grid-item" item-data="{data.json_string}">
+    <div class="inner">
+
+      <div class="image" style="cursor:pointer;">
+
+      </div>
+      <h3>
+  {data.title}
+  </h3>
+    </div>
+  </div>
+    <!-- END data -->
+  </div>
+  </div>
+        </div>
+        <!-- BEGIN marks -->
+
+        <!-- END marks -->
+    </div>
+</div>
+<div id="add_new" class="modal">
+  <form class="modal">
+    <fieldset>
+      <input id="form_title" name="title" type="text" required/>
+      <label for="form_title">Title</label>
+    </fieldset>
+    <fieldset>
+      <input id="form_image" name="image" type="file" required/>
+      <label for="form_image" style="display:none;">Image</label>
+    </fieldset>
+    <fieldset>
+      <input id="form_author" name="author" type="text" required/>
+      <label for="form_author">Author</label>
+    </fieldset>
+    <fieldset>
+    <textarea id="form_description" name="description" type="text" rows="3" required></textarea>
+      <label for="form_description">Description</label>
+    </fieldset>
+    <fieldset>
+      <select id="form_type" name="type" >
+      <option value="anime">Anime</option>
+      <option value="manga">Manga</option>
+      </select>
+      <label for="form_type">Type</label>
+    </fieldset>
+    <input type="submit" class="button"/>
+  </form>
+</div>
+<div class="template well hidden">
+    <form>
+        <span class="pull-right"><i class="fa fa-times pointer"></i></span>
+
+        <label>Name
+        <input type="text" class="form-control" name="name" value="" />
+        </label>
+
+        <label>regex
+        <input type="text" class="form-control" name="regex" value="" />
+        </label>
+        <label>Replacement
+        <input type="text" class="form-control" name="replacement" value="" />
+        </label>
+    </form>
+</div>
+
+<button class="btn btn-lg btn-success pull-right" id="add"><i class="fa fa-plus"></i> Add New Mark</button>
+
+<button id="save" class="floating-button mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
+    <i class="material-icons">save</i>
+</button>
+<script src="//npmcdn.com/masonry-layout@4.0.0/dist/masonry.pkgd.min.js"></script>
+
+
+<script>
+
+    document.querySelector('#add_new').style.display = 'none';
+    document.querySelector('#add').addEventListener('click', function() {
+Modal.open({
+  content: document.querySelector('#add_new').innerHTML,
+  draggable: true,
+  width: '300px',
+  openCallback: function() {
+    //alert('This is an example of the use of openCallback');
+  }
+});
+});
+    $('#modal-container form').on('submit', function(ev) {
+        var arr = [];
+        $('#lists .grid-item').each(function() {
+            var data = $(this).attr('iitem-data');
+
+                arr.push(data);
+
+        });
+        socket.emit('admin.settings.saveMarks', arr, function() {
+            app.alertSuccess('Custom marks saved and activated');
+        });
+    });
+</script>
